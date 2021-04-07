86eee9ea4b534c335d42a50a291cb064e6434b6c
kartzz123 <kartzz282@gmail.com>
2021-04-07 21:50:27 +0530

    my python commit

---
+++ python.py
@@ -0,0 +1,8 @@
+#!/USr/local/bin/python3.8.5
+
+l1 = ["1","2","3"]
+l2 = ["ram","sham"]
+
+print(l1,l2)
+
+
---
+++ raaj.py
@@ -0,0 +1,31 @@
+#!/usr/local/bin/python3.8.5
+
+a =10
+b=20
+
+#if a<b :
+ #   print ("yes")
+#else:
+ #   print ("no")
+
+if a>b :
+    print ("yes")
+else:
+    print ("no")
+
+if a==b :
+    print ("it is")
+else:
+    print ("it is not")
+
+if a!=b :
+    print ("it is virtusa")
+else:
+    print ("it is not")
+
+if a >=b:
+    print ("yes")
+else:
+    print ("no")
+
+
